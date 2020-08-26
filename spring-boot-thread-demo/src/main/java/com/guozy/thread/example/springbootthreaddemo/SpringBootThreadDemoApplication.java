89ebdebf868dbd9bd3a9bdb1a90cc75545cd9067
guozy20 <guoziyang20@163.com>
2020-08-26 15:16:47 +0800

    并发编程-1、多线程的意义和使用

---
+++ spring-boot-thread-demo/src/main/java/com/guozy/thread/example/springbootthreaddemo/SpringBootThreadDemoApplication.java
@@ -0,0 +1,13 @@
+package com.guozy.thread.example.springbootthreaddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThreadDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootThreadDemoApplication.class, args);
+    }
+
+}
