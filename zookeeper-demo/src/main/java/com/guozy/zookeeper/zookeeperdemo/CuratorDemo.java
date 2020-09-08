5e344474afe65f289a969100d8b7cb85d0a96be2
guozy20 <guoziyang20@163.com>
2020-09-08 22:27:02 +0800

    zookeeper-api联系

--- zookeeper-demo/src/main/java/com/guozy/zookeeper/zookeeperdemo/CuratorDemo.java
+++ zookeeper-demo/src/main/java/com/guozy/zookeeper/zookeeperdemo/CuratorDemo.java
@@ -5,19 +5,32 @@ import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.api.BackgroundCallback;
 import org.apache.curator.framework.api.CuratorEvent;
+import org.apache.curator.framework.recipes.cache.ChildData;
+import org.apache.curator.framework.recipes.cache.NodeCache;
+import org.apache.curator.framework.recipes.cache.NodeCacheListener;
+import org.apache.curator.framework.recipes.cache.PathChildrenCache;
+import org.apache.curator.framework.recipes.cache.PathChildrenCacheEvent;
+import org.apache.curator.framework.recipes.cache.PathChildrenCacheListener;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.data.ACL;
 import org.apache.zookeeper.data.Id;
+import org.apache.zookeeper.data.Stat;
 import org.apache.zookeeper.server.auth.DigestAuthenticationProvider;
 
-import java.security.NoSuchAlgorithmException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
 public class CuratorDemo {
+    ZooKeeper zooKeeper = null;
+
     public static void main(String[] args) throws Exception {
         List<AuthInfo> authInfos = new ArrayList<>();
         AuthInfo authInfo = new AuthInfo("digest", "u2:u2".getBytes());
@@ -38,15 +51,17 @@ public class CuratorDemo {
 
 //        operatorWithAsync(curatorFramework);
 //        setAcl(curatorFramework);
-        get(curatorFramework);
+//        get(curatorFramework);
+//        addNodeCacheListener(curatorFramework, "/first_auth");
+        addPathChildCacheListener(curatorFramework, "/first_auth");
+        System.in.read();
     }
 
-
     private static String create(CuratorFramework curatorFramework) throws Exception {
-        curatorFramework.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT)
+        String rs = curatorFramework.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT)
                 .forPath("/first_111", "hello guozy".getBytes());
 
-        return null;
+        return rs;
 
     }
 
@@ -89,7 +104,7 @@ public class CuratorDemo {
         acls.add(acl2);
 
         curatorFramework.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT)
-                .withACL(acls).forPath("/first_auth","123123".getBytes());
+                .withACL(acls).forPath("/first_auth", "123123".getBytes());
 
         return null;
 
@@ -99,5 +114,82 @@ public class CuratorDemo {
         String rsl = new String(curatorFramework.getData().forPath("/first_auth"));
         System.out.println(rsl);
         return rsl;
+    }
+
+    /**
+     * PathChild
+     * Node
+     * Tree
+     */
+    private static void addNodeCacheListener(CuratorFramework curatorFramework, String path) throws Exception {
+
+        NodeCache nodeCache = new NodeCache(curatorFramework, path, false);
+        NodeCacheListener nodeCacheListener = new NodeCacheListener() {
+            @Override
+            public void nodeChanged() throws Exception {
+                System.out.println("Receive Node Change");
+                System.out.println("" + nodeCache.getCurrentData().getPath() + "->" + new String(
+                        nodeCache.getCurrentData().getData()));
+            }
+        };
+        nodeCache.getListenable().addListener(nodeCacheListener);
+        nodeCache.start();
+    }
+
+
+    private static void addPathChildCacheListener(CuratorFramework curatorFramework, String path) throws Exception {
+        PathChildrenCache pathChildrenCache = new PathChildrenCache(curatorFramework, path, true);
+        PathChildrenCacheListener pathChildrenCacheListener = new PathChildrenCacheListener() {
+            @Override
+            public void childEvent(CuratorFramework curatorFramework,
+                                   PathChildrenCacheEvent pathChildrenCacheEvent) throws Exception {
+                System.out.println("子节点事件变更的回调");
+                ChildData childData = pathChildrenCacheEvent.getData();
+                System.out.println(childData.getPath() + "->" + new String(childData.getData()));
+            }
+        };
+        pathChildrenCache.getListenable().addListener(pathChildrenCacheListener);
+        pathChildrenCache.start(PathChildrenCache.StartMode.NORMAL);
+    }
+
+    public void originApiTest() throws IOException, KeeperException, InterruptedException {
+        zooKeeper = new ZooKeeper("192.168.124.12:2181,192.168.124.14:2181,192.168.128.12:2181",
+                5000,
+                new Watcher() {
+                    @Override
+                    public void process(WatchedEvent event) {
+                        // event 表示连接成功之后会产生的回调事件
+                    }
+                });
+        Stat stat = new Stat();
+       /* zooKeeper.getData("/first_auth", new Watcher() {
+            @Override
+            public void process(WatchedEvent event) {
+                // 事件回调
+            }
+        }, stat);  // 针对当前节点*/
+
+        zooKeeper.getData("/first_auth", new DataWatcherListener(), stat);  // 针对当前节点
+
+
+       /* zooKeeper.exists();  // 针对当前节点
+        zooKeeper.getChildren(); // 针对子节点的监听*/
+
+    }
+
+    class DataWatcherListener implements Watcher {
+
+        @Override
+        public void process(WatchedEvent event) {
+            String path = event.getPath();
+            // 再次注册监听
+            try {
+                zooKeeper.getData(path, this, new Stat());
+            } catch (KeeperException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
